42ea51472533b889c41351e969eb917fc3b29252
Adam Zambrzycki <adam@zambrzycki.net>
2022-10-21 02:07:43 +0200

    Remove pydantic from shapes

--- transitsnake/shapes.py
+++ transitsnake/shapes.py
@@ -1,20 +1,26 @@
+from dataclasses import dataclass
 from typing import ClassVar, Optional, List
 
-from pydantic import BaseModel, NonNegativeInt, NonNegativeFloat
-
-from . import BaseDatasetType
+from . import BaseDatasetType, Field
 from .types import Point
+from .validation import non_negative
 
 
-class Shape(BaseModel, BaseDatasetType):
+@dataclass
+class Shape(BaseDatasetType):
     filename: ClassVar[str] = 'shapes.txt'
     primary_key: ClassVar[tuple] = ('shape_id', 'shape_pt_sequence')
 
     shape_id: str
     shape_pt_lat: float
     shape_pt_lon: float
-    shape_pt_sequence: NonNegativeInt
-    shape_dist_traveled: Optional[NonNegativeFloat]
+    shape_pt_sequence: int
+    shape_dist_traveled: Optional[float] = None
+
+    _meta = {
+        'shape_pt_sequence': Field(validators=non_negative),
+        'shape_dist_traveled': Field(validators=non_negative),
+    }
 
     @staticmethod
     def path(shape_id: str, points: List[Point]):
