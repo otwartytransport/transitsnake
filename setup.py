4ce39ba32506739a312b1a81b68898074054b763
Adam Zambrzycki <adam@zambrzycki.net>
2022-10-31 21:50:30 +0100

    Add details to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='transitsnake',
-    version='0.1',
+    version='0.0.1',
+    url='https://github.com/otwartytransport/transitsnake',
     packages=['transitsnake'],
     license='Apache License 2.0',
-    author='Adikso',
+    author='otwartytransport.pl and contributors',
+    author_email='contact@otwartytransport.pl',
     description='Library for creating and reading GTFS files',
     install_requires=[
         'validators~=0.20.0'
